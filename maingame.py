ba1ea45d66258a2163d827256c702675a412bd79
Magdalena Szymkowiak <262309@student.pwr.edu.pl>
2021-06-08 16:29:42 +0200

    cleaning

--- maingame.py
+++ maingame.py
@@ -2,14 +2,7 @@ from breakout import Breakout
 
 if __name__ == "__main__":
     game = Breakout()
-    #secure version
-    #while True:  #this could change somehow
-     #   game.handle_events()
-      #  game.update()
-       # game.draw()
     while game.running:
         game.current_menu.show_menu()
-        #game.playing = True
         game.game_loop()
-        #game.breakout_loop()
 
